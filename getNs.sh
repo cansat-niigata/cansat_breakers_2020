d4513a1f9579f3334e018552262efa73a5bd37f9
shinjin88 <49982287+shinjin88@users.noreply.github.com>
2020-12-20 11:51:46 +0900

    Add files via upload

--- getNs.sh
+++ getNs.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-#改行コードに注意!
-#通常WSL1からは名前解決できないけどこれを使えばsshとUSBシリアル通信がWSL上で完結できるゾ！
-#ついでに背景を透過できて便利
+
 
 CMDNAME=`basename $0`
+FLAG_U=0
 
-while getopts u:h: OPT
+
+while getopts u:h OPT
 do
 	case $OPT in
-		"u" ) FLAG_U=1 ; VALUE_UNAME=$OPTARG ;;
-		"h" ) VALUE_HNAME=$OPTARG ;;
-		* ) echo "Usage: $CMDNAME [-u username] [-h hostname]" 1>&2
-		    exit 1 ;;
+		u ) FLAG_U=1 ; VALUE_UNAME=$OPTARG ;;
+		h ) echo "Usage: $CMDNAME [-u username] hostname" 1>&2
+		exit 1 ;;
 	esac
 done
 
+shift $(($OPTIND - 1))
+VALUE_HNAME=$1
+
+if [ -z $VALUE_HNAME ]; then
+	echo "Usage: $CMDNAME [-u username] hostname" 1>&2
+	exit 1
+fi
+
 if [ $FLAG_U = 1 ]; then
-	TMP=`powershell.exe "Resolve-DnsName $VALUE_HNAME | ConvertTo-Json" | jq -r '.[0].IPAddress'`
+	TMP=`powershell.exe "Resolve-DnsName $VALUE_HNAME | ConvertTo-Json" | jq -r '.[1].IP4Address'`
 	ssh $VALUE_UNAME@$TMP
 else
-	powershell.exe "Resolve-DnsName $VALUE_HNAME | ConvertTo-Json" | jq -r '.[0].IPAddress'
+	powershell.exe "Resolve-DnsName $VALUE_HNAME | ConvertTo-Json" | jq -r '.[1].IP4Address'
 fi
